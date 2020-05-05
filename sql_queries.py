6868790cce257e1c487bb9e42949361a9e2cf124
Nitin Shambhurao Patil <p.nitin15@gmail.com>
2020-05-05 16:17:22 +0530

    Query lists code

--- sql_queries.py
+++ sql_queries.py
@@ -174,3 +174,11 @@ EXTRACT (WEEK FROM a.start_time), EXTRACT (MONTH FROM a.start_time),
 EXTRACT (YEAR FROM a.start_time), EXTRACT (WEEKDAY FROM a.start_time) FROM
 (SELECT TIMESTAMP 'epoch' + start_time/1000 *INTERVAL '1 second' as start_time FROM songplays) a;
 """)
+
+
+# QUERY LISTS
+
+create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+copy_table_queries = [staging_events_copy, staging_songs_copy]
+insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
