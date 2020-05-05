d2d39470aa999000a13b03258eb6cf6921b3a6b7
Nitin Shambhurao Patil <p.nitin15@gmail.com>
2020-05-05 16:08:45 +0530

    Drop Table code

---
+++ sql_queries.py
@@ -0,0 +1,9 @@
+# DROP TABLES
+
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events;"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs;"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
